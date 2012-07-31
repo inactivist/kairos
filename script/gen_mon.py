44c1069eeec320af0782ee06ed20d3155fc94482
inactivist <inactivist@gmail.com>
2012-07-31 05:47:12 -0700

    Adding --limit arg to control depth of histogram display, and try to delete existing keys on startup.

--- script/gen_mon.py
+++ script/gen_mon.py
@@ -7,18 +7,17 @@ import redis
 import time
 import datetime
 import random
+from argparse import ArgumentError
 
 MAX_WIDTH_COLUMNS = 60
 
-KEY = 'example.com'
-KEY_PREFIX = 'timedata:domain'
-
-client = redis.Redis('localhost', 6379)
-
 class EventCounter(object):
     """Manage event counters."""
-    def __init__(self, redis_client, key_prefix=None):
+    def __init__(self, redis_client, key_prefix):
         """Create our counter structure."""
+        if not key_prefix:
+            raise ValueError('key_prefix must be provided and length must be >0')
+        
         self.counters = Timeseries(redis_client, {
                 'minute': {
                         'step': 60,              # 60 seconds
@@ -37,6 +36,8 @@ class EventCounter(object):
                     },
             }, 
             key_prefix=key_prefix)
+        self.key_prefix = key_prefix
+        self.redis_client = redis_client
 
     def record_hit(self, item_id):
         """Record a hit.  All this does is increments the current time bucket."""
@@ -54,7 +55,14 @@ class EventCounter(object):
         """Return the sum of the counts for the most recent periods."""
         return sum(self.get_counts(interval_name, item_id, n_recent_periods))
     
+    def delete_keys(self):
+        """Clean up (delete) all keys used by this instance."""
+        raise NotImplementedError() 
     
+    
+KEY = 'example.com'
+KEY_PREFIX = 'timedata:domain'
+client = redis.Redis('localhost', 6379)
 counters = EventCounter(redis_client=client, key_prefix=KEY_PREFIX)
 
 def hit(domain):
@@ -80,6 +88,12 @@ def sum_series(series):
     return sum(series.values())
 
 def generate():
+    # Clear any existing keys.
+    try:
+        counters.delete_keys()
+    except NotImplementedError:
+        print "EventCounter.delete_keys not implemented, ignoring."
+        
     # record a couple of hits.
     hit(KEY)
     hit(KEY)
@@ -95,17 +109,17 @@ def generate():
 
 interval_max_values = { 'minute' : 100, 'hour': 2000, 'daily': 2000*24 }
 
-def monitor(interval_name):
+def monitor(interval_name, limit=5):
     while True:
-        series = counters.get_counts(interval_name, KEY, 15)
+        series = counters.get_counts(interval_name, KEY, limit)
         sum = counters.get_sum(interval_name, KEY, 5)
         #series = counters.series(KEY, interval_name)
         #last_5 = counters.series(KEY, interval_name, steps=5, condensed=False)
         # sum = sum_series(last_5)
         # This should work but breaks: condensed = counters.series(KEY, interval_name, steps=5, condensed=True)
         #dump_series(time.time(), series)
-        plot_series(series, interval_max_values[interval_name])
-        print "%d in last 5 %s (~%2.2f per %s)." % (sum, interval_name, sum/5.0, interval_name)
+        plot_series(series, interval_max_values[interval_name], limit=limit)
+        print "%d in last %d %s (~%2.2f per %s)." % (sum, limit, interval_name, sum/5.0, interval_name)
         time.sleep(1)
 
 if __name__ == "__main__":
@@ -113,8 +127,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Test something.')
     parser.add_argument('op', metavar='op', help='If op=generate, generates random data.  If op=monitor, monitors generated data.', default='generate', action="store", choices=['generate', 'monitor'])
     parser.add_argument('-i', '--interval', metavar='interval', default='minute', action='store', choices=['minute', 'hour', 'daily'])
+    parser.add_argument('-l', '--limit', metavar='limit', default=15, action='store', type=int, help='The number of past entries to display in histogram in monitor mode.')
     opts = parser.parse_args()
     if opts.op == 'generate':
         generate()
     else:
-        monitor(opts.interval)
+        monitor(opts.interval, opts.limit)
